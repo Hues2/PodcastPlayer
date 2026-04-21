ba9ae34b4d617f89f713cfe3b954072017114875
Greg Ross <gregandrewross79@gmail.com>
2026-04-21 16:40:01 +0100

    Initial push

---
+++ PodcastPlayer/PodcastPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastPlayerApp.swift
+//  PodcastPlayer
+//
+//  Created by Greg Ross on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PodcastPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PodcastPlayerTests/PodcastPlayerTests.swift
@@ -0,0 +1,19 @@
+//
+//  PodcastPlayerTests.swift
+//  PodcastPlayerTests
+//
+//  Created by Greg Ross on 21/04/2026.
+//
+
+import Testing
+@testable import PodcastPlayer
+
+struct PodcastPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
